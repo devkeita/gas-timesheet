0a1e8fea8782b80b9a809d4287d03ad34636c542
keita <aoyamakeita@aoyamakeidainoMacBook-Pro.local>
2020-07-22 00:21:41 +0900

    make container and fixed env

---
+++ src/types.ts
@@ -0,0 +1,7 @@
+const TYPES = {
+    SlackIncomingUrl: Symbol.for("SlackIncomingUrl"),
+    RequestFactory: Symbol.for("RequestFactory"),
+    ResponseHandler: Symbol.for("ResponseHandler"),
+}
+
+export {TYPES};
