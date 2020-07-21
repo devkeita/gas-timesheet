266ba08d8164e947bbca5c8a77b088bf74bb1729
keita <aoyamakeita@aoyamakeidainoMacBook-Pro.local>
2020-07-21 18:13:23 +0900

    fixed resposen and request

--- src/slack/requestFactory.ts
+++ src/slack/requestFactory.ts
@@ -1,19 +1,26 @@
+import Request from "../request";
+
 export default class RequestFactory {
     constructor() {}
 
-    factory(e) {
-        let params = JSON.parse(e.PostData.getDataAsString());
+    factory(e): Request {
+        const params = JSON.parse(e.postData.getDataAsString());
 
         if (params.type === 'url_verification') {
             // challengeをresponseとして返す
+            return null;
         }
 
         if (params.type !== 'event_callback' || params.event.type !== 'message') {
             // イベントがメッセージじゃなかったら反応しない
+            return null;
         }
 
         if (params.event.bot_id) {
             // メッセージがbotからだったら反応しない
+            return null;
         }
+
+        return new Request(params.event.text);
     }
 }
--- src/slack/responseHandler.ts
+++ src/slack/responseHandler.ts
@@ -1,17 +1,19 @@
 import URLFetchRequestOptions = GoogleAppsScript.URL_Fetch.URLFetchRequestOptions;
 
-export default class ResponseHandler {
-    readonly slackIncomingURL = process.env.SLACK_INCOMING_WEBHOOK_URL
+import Response from "../response";
 
-    handle() {
+export default class ResponseHandler {
+    private slackIncomingURL = process.env.SLACK_INCOMING_WEBHOOK_URL
+
+    handle(response: Response) {
         const payload = {
-            text: 'hello'
+            text: response.content
         };
 
         const send_options: URLFetchRequestOptions = {
             method: 'post',
             payload: JSON.stringify(payload)
-        }
+        };
 
         if (this.slackIncomingURL) {
             UrlFetchApp.fetch(this.slackIncomingURL, send_options);
